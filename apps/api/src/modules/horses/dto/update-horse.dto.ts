96d58fb5dccb78a3522e4dfad3cae657a1d1775d
jmcisnero <jmcisnero.bkp@gmail.com>
2026-02-26 17:23:39 -0300

    Update update-horse.dto.ts

--- apps/api/src/modules/horses/dto/update-horse.dto.ts
+++ apps/api/src/modules/horses/dto/update-horse.dto.ts
@@ -1,5 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateHorseDto } from './create-horse.dto';
-
-// Hereda todas las validaciones de CreateHorseDto pero las hace opcionales
 export class UpdateHorseDto extends PartialType(CreateHorseDto) {}
