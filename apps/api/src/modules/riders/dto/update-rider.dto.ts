f36048807481b8e64e8c59bc3c10ac7958f612f2
jmcisnero <jmcisnero.bkp@gmail.com>
2026-02-26 17:24:52 -0300

    Update update-rider.dto.ts

--- apps/api/src/modules/riders/dto/update-rider.dto.ts
+++ apps/api/src/modules/riders/dto/update-rider.dto.ts
@@ -1,5 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateRiderDto } from './create-rider.dto';
-
-// Hereda todas las validaciones de CreateRiderDto pero las hace opcionales
 export class UpdateRiderDto extends PartialType(CreateRiderDto) {}
