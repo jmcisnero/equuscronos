71e73a509a6e144d0b720c0b93516cf8cadf0e4b
jmcisnero <jmcisnero.bkp@gmail.com>
2026-05-04 00:15:14 -0300

    Create WeightControlsModule with necessary imports

---
+++ apps/api/src/modules/weight-controls/weight-controls.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WeightControlsService } from './weight-controls.service';
+import { WeightControlsController } from './weight-controls.controller';
+import { WeightControl } from './entities/weight-control.entity';
+import { CompetitionEntry } from '../competition-entries/entities/competition-entry.entity';
+import { Stage } from '../competitions/entities/stage.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([WeightControl, CompetitionEntry, Stage])],
+  controllers: [WeightControlsController],
+  providers: [WeightControlsService],
+})
+export class WeightControlsModule {}
