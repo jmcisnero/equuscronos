d455f9dd736c16224d63e2587e91d713bc138e19
jmcisnero <jmcisnero.bkp@gmail.com>
2026-05-04 00:32:55 -0300

    Update VetInspection import path in module

--- apps/api/src/modules/vet-inspections/vet-inspections.module.ts
+++ apps/api/src/modules/vet-inspections/vet-inspections.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VetInspectionsService } from './vet-inspections.service';
 import { VetInspectionsController } from './vet-inspections.controller';
-import { VetInspection } from '../competitions/entities/vet-inspection.entity';
+import { VetInspection } from './entities/vet-inspection.entity';
 import { TimingRecord } from '../competitions/entities/timing-record.entity';
 
 @Module({
